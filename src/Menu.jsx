566abeb5f40de06dd2bd6dbe4eda8eb4ffb756d3
Amir Boisov <amir.boisov@gmail.com>
2024-03-17 19:00:35 +0300

    responsive menu 50%

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,22 +1,36 @@
-import { Component } from 'react';
-// import { Component } from './News';
 import './styles/menu.scss';
+import { useState } from 'react';
 
 
 
-export default class Menu extends Component {
-    render() {
-        return <div className='Menu-container'>
-            <a className='Menu-logo' href="/"><img src="../Vector.jpg" alt=""/></a>
-            <div className='Menu-links'>
-                <a href="/news">Новости</a>
-                <a href="/questions">Вопросы</a>
-                <a href="/knowledgebase">База знаний</a>
-                <a href="/companies">Компании</a>
+export default function Menu() {
+        const [ isActive, setActive ] = useState(false);
+        const toggleClass = () => {
+            setActive(!isActive)
+        }
+
+        return <div className='Menu-mobile'>
+            <div className="three col">
+                <div className={`hamburger ${isActive ? "is-active" : ""}`} id="hamburger-1" onClick={toggleClass}>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+        </div>
+            <div className='Menu-container'>
+                <a className='Menu-logo' href="/"><img src="../Vector.jpg" alt=""/></a>
+                <div className='Menu-links'>
+                    <a href="/news">Новости</a>
+                    <a href="/questions">Вопросы</a>
+                    <a href="/knowledgebase">База знаний</a>
+                    <a href="/companies">Компании</a>
+            </div>
             </div>
         </div>
-    }
 }
+
+
+
 
 
 
